db10417e70a2f8e3ed73b7284d0e5a8f0766015c
Bat-Code <jdmacias.3110@gmail.com>
2023-10-13 02:45:06 -0500

    El programa ya es funcional y es capaz de organizar los archivos

--- script.py
+++ script.py
@@ -9,10 +9,20 @@ def createDirectorios(nombre:str, ruta:str):
         ruta_completa = ruta + "/" + nombre
         os.makedirs(ruta_completa, exist_ok=True)
 
-def getExtension(direccion):
+def getExtension(direccion:str, carpetasAndExtensiones:dict):
     for archivo in os.listdir(direccion):
-        extension = os.path.splitext(archivo)[1]
-        extension = extension[1:]  # aqui se quita el punto de la extension
+        ruta_completa = os.path.join(direccion, archivo)
+        # Verifica si es un archivo y no una carpeta
+        if os.path.isfile(ruta_completa):
+            extension = os.path.splitext(archivo)[1]
+            extension = extension[1:]  # aquí se quita el punto de la extensión
+            for carpeta, extensiones in carpetasAndExtensiones.items():
+                if extension in extensiones:
+                    # Mueve ek archivo a la carpeta correspondiente
+                    carpetaDestino = os.path.join(direccion, carpeta)
+                    if not os.path.exists(carpetaDestino):
+                        createDirectorios(carpeta, direccion)
+                    shutil.move(ruta_completa, os.path.join(carpetaDestino, archivo))
 
 # Estas son las carpetas que se tienen que crear
 carpetasAndExtensiones = {
@@ -36,11 +46,11 @@ carpetasAndExtensiones = {
     ]
 }
 
-xd = list(carpetasAndExtensiones.keys()) # Asi sacare el nombre de las carpetas del diccionario
 direccion = "C:/Users/juand/Downloads/prueba"
+xd = list(carpetasAndExtensiones.keys()) # Asi sacare el nombre de las carpetas del diccionario
 
 for i in range(len(xd)): # Aqui se crean las carpetas
     createDirectorios(xd[i],direccion)
 
-
+getExtension(direccion, carpetasAndExtensiones)
 
