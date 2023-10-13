bf302bd1222597f56cc94b0eaddcd14e7bae6c0a
Bat-Code <jdmacias.3110@gmail.com>
2023-10-13 03:00:46 -0500

    Se ha agregado el input al script y corrigiendo erroes al agregarlo

--- script.py
+++ script.py
@@ -2,14 +2,12 @@ import os
 import shutil
 
 #Funcion para crear las carpetas
-def createDirectorios(nombre:str, ruta:str):
-    for raiz, dirs, archivos in os.walk(ruta):
-        dirs[:] = [] # Aqui evito que lea los subdirectorios
+def createDirectorios(nombre: str, ruta: str, dirs: list):
     if nombre not in dirs:
         ruta_completa = ruta + "/" + nombre
         os.makedirs(ruta_completa, exist_ok=True)
 
-def getExtension(direccion:str, carpetasAndExtensiones:dict):
+def getExtension(direccion:str, carpetasAndExtensiones:dict, dirs: list):
     for archivo in os.listdir(direccion):
         ruta_completa = os.path.join(direccion, archivo)
         # Verifica si es un archivo y no una carpeta
@@ -21,7 +19,7 @@ def getExtension(direccion:str, carpetasAndExtensiones:dict):
                     # Mueve ek archivo a la carpeta correspondiente
                     carpetaDestino = os.path.join(direccion, carpeta)
                     if not os.path.exists(carpetaDestino):
-                        createDirectorios(carpeta, direccion)
+                        createDirectorios(carpeta, direccion, dirs)
                     shutil.move(ruta_completa, os.path.join(carpetaDestino, archivo))
 
 # Estas son las carpetas que se tienen que crear
@@ -46,11 +44,15 @@ carpetasAndExtensiones = {
     ]
 }
 
-direccion = "C:/Users/juand/Downloads/prueba"
+direccion = input("Ingrese la direccion: ")
 xd = list(carpetasAndExtensiones.keys()) # Asi sacare el nombre de las carpetas del diccionario
 
-for i in range(len(xd)): # Aqui se crean las carpetas
-    createDirectorios(xd[i],direccion)
+# Obtener la lista de directorios raíz para evitar UnboundLocalError
+for raiz, dirs, archivos in os.walk(direccion):
+    break
 
-getExtension(direccion, carpetasAndExtensiones)
+for i in range(len(xd)): # Aqui se crean las carpetas
+    createDirectorios(xd[i],direccion, dirs)
+
+getExtension(direccion, carpetasAndExtensiones, dirs)
 
